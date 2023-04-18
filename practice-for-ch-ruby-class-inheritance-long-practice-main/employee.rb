fb7d6d58a03c1d989a78428b70e7d8107f6c4817
LetsGitFunky <NickHeinBass@gmail.com>
2023-04-18 13:47:05 -0700

    initialize Employee

--- practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/employee.rb
@@ -1,3 +1,11 @@
-class Employee
 
+class Employee
+    def initialize(name, title)
+        @name = name
+        @title = title
+        @salary # hash[title] =
+        @boss # .parent
+    end
 end
+
+p tim = Employee.new("Tim", "Barista")
--- practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
+++ practice-for-ch-ruby-class-inheritance-long-practice-main/manager.rb
@@ -1,3 +1,7 @@
-class Manager
+require_relative "employee.rb"
 
+class Manager < Employee
+    def some_method
+        puts "I'm a method"
+    end
 end
